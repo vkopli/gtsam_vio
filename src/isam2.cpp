d97ade5dc4028cc838173e6d013bfd8c9cf08f3d
vkopli <vkopli@seas.upenn.edu>
2019-02-05 17:30:19 -0500

    initialize gtsam factor graph in isam2 node

--- src/isam2.cpp
+++ src/isam2.cpp
@@ -1,4 +1,7 @@
-// SUBSCRIBER NODE ("listener")
+// SUBSCRIBER NODE ("isam2")
+
+// ROS/PACKAGE INCLUDES
+/* ************************************************************************* */
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -9,19 +12,65 @@
 #include <legged_vio/CameraMeasurement.h>
 #include <sensor_msgs/Imu.h>
 
+// ISAM2 INCLUDES
+/* ************************************************************************* */
+
+// Camera observations of landmarks (i.e. pixel coordinates) will be stored as Point2 (x, y).
+#include <gtsam/geometry/Point2.h>
+
+// Each variable in the system (poses and landmarks) must be identified with a unique key.
+// We can either use simple integer keys (1, 2, 3, ...) or symbols (X1, X2, L1).
+// Here we will use Symbols
+#include <gtsam/inference/Symbol.h>
+
+// We want to use iSAM2 to solve the structure-from-motion problem incrementally, so
+// include iSAM2 here
+#include <gtsam/nonlinear/ISAM2.h>
+
+// iSAM2 requires as input a set set of new factors to be added stored in a factor graph,
+// and initial guesses for any new variables used in the added factors
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/Values.h>
+
+// In GTSAM, measurement functions are represented as 'factors'. Several common factors
+// have been provided with the library for solving robotics/SLAM/Bundle Adjustment problems.
+// Here we will use Projection factors to model the camera's landmark observations.
+// Also, we will initialize the robot at some location using a Prior factor.
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/ProjectionFactor.h>
+
+// ADDITIONAL INCLUDES
+/* ************************************************************************* */
+
+#include <vector>
+
 using namespace message_filters;
 using namespace legged_vio;
 using namespace sensor_msgs;
+using namespace gtsam;
 
+// GLOBAL VARIABLES
+/* ************************************************************************* */
+
+// Create a Factor Graph and Values to hold the new data, accessible from callback function
+NonlinearFactorGraph graph;
+Values initialEstimate;
+
+
+// FUNCTIONS
+/* ************************************************************************* */
 void callback(const CameraMeasurementConstPtr& features, const ImuConstPtr& imu) {
-  //
+  // use features to estimate feature locations in camera frame
+  // 
 }
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   //ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
+/* ************************************************************************* */
 int main(int argc, char **argv) {
+
 
   ros::init(argc, argv, "isam2_node"); // specify name of node and ROS arguments
 
