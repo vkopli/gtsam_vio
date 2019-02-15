e8fd4a97b471973abf2a6c4cd63047929890300e
vkopli <vkopli@seas.upenn.edu>
2019-02-15 18:35:30 -0500

    finished general framework for ISAM2

--- src/isam2.cpp
+++ src/isam2.cpp
@@ -15,8 +15,8 @@
 // ISAM2 INCLUDES
 /* ************************************************************************* */
 
-// Camera observations of landmarks (i.e. pixel coordinates) will be stored as Point2 (x, y).
-#include <gtsam/geometry/Point2.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Cal3_S2Stereo.h>
 
 // Each variable in the system (poses and landmarks) must be identified with a unique key.
 // We can either use simple integer keys (1, 2, 3, ...) or symbols (X1, X2, L1).
@@ -34,10 +34,8 @@
 
 // In GTSAM, measurement functions are represented as 'factors'. Several common factors
 // have been provided with the library for solving robotics/SLAM/Bundle Adjustment problems.
-// Here we will use Projection factors to model the camera's landmark observations.
-// Also, we will initialize the robot at some location using a Prior factor.
 #include <gtsam/slam/PriorFactor.h>
-#include <gtsam/slam/ProjectionFactor.h>
+#include <gtsam/slam/StereoFactor.h>
 
 // ADDITIONAL INCLUDES
 /* ************************************************************************* */
@@ -59,95 +57,141 @@ class Callbacks {
 
 private:
   
+	int frame = 0;
+
   // Hold node handle initialized in main
   shared_ptr<ros::NodeHandle> nh_ptr;
   
-  // Create a Factor Graph and Values to hold the new data, accessible from callback function
+  // Create a Factor Graph and Values to hold the new data
   NonlinearFactorGraph graph;
-  Values initialEstimate;  
+  Values initial_estimate; // initial estimates of feature locations (factors)
 
   // --> Create iSAM2 object
   unique_ptr<ISAM2> isam;
 
+	// Camera calibration intrinsics
+	double f;
+	double cx;
+	double cy;
+
   // Camera calibration intrinsic matrix
-  Cal3_S2::shared_ptr K;
+  Cal3_S2Stereo::shared_ptr K;
 
   // Camera calibration extrinsic
   double Tx; // distance from cam0 to cam1
 
   // --> Camera observation noise model (has to do with IMU?)
-  noiseModel::Isotropic::shared_ptr measurementNoise = noiseModel::Isotropic::Sigma(2, 1.0); // one pixel in u and v
+  noiseModel::Isotropic::shared_ptr noise_model = 
+			noiseModel::Isotropic::Sigma(2, 1.0); // one pixel in u and v
 
 public:
  
   Callbacks(shared_ptr<ros::NodeHandle> nh_ptr_copy) : nh_ptr(move(nh_ptr_copy)) {
 
-    // Initialize camera calibration matrix using YAML file
+    // YAML intrinsics (pinhole): [fu fv pu pv]
     vector<int> cam0_intrinsics(4);
     vector<int> cam1_intrinsics(4);
-    // YAML intrinsics (pinhole): [fu fv pu pv]
     nh_ptr->getParam("cam0/intrinsics", cam0_intrinsics); 
     nh_ptr->getParam("cam1/intrinsics", cam1_intrinsics);
-    // GTSAM Cal3_S2 (doubles): (fx, fy, s, u0, v0)
-    Cal3_S2::shared_ptr K(new Cal3_S2(cam0_intrinsics[0], cam0_intrinsics[1], 0.0, 
-                                      cam0_intrinsics[2], cam0_intrinsics[3])); 
-		// Stereo Camera Extrinsic (distance between 2 cameras)
+		this->f = (cam0_intrinsics[0] + cam0_intrinsics[1]) / 2;
+		// neglecting image center of right camera...
+		this->cx = cam0_intrinsics[2];
+		this->cy = cam0_intrinsics[3];
+		
+    // K: (fx, fy, s, u0, v0, b) (b: baseline where Z = f*d/b; Tx is negative)
+		Cal3_S2Stereo::shared_ptr K(new Cal3_S2Stereo(cam0_intrinsics[0], 
+			cam0_intrinsics[1], 0.0, cx, cy, -Tx)); 
+		
+    // YAML extrinsics (distance between 2 cameras)
 		vector<double> T_cam1(16);
 		nh_ptr->getParam("cam1/T_cn_cnm1", T_cam1);
-		Tx = T_cam1[3];
-
-		ROS_INFO("cam0/intrinsics exists? %d", nh_ptr->hasParam("cam0/intrinsics")); 
-		ROS_INFO("intrinsics: %d, %d, %d, %d", cam0_intrinsics[0], cam0_intrinsics[1], cam0_intrinsics[2], cam0_intrinsics[3]);
-    ROS_INFO("Tx: %f, %f, %f, %f", T_cam1[0], T_cam1[1], T_cam1[2], T_cam1[3]);
+		this->Tx = T_cam1[3];
     
-    // iSAM2 performs partial relinearization/variable reordering at each step A parameter
-    // parameter structure allows setting of properties: relinearization threshold & type of linear solver
-    // this example: set the relinearization threshold small so the iSAM2 result will approach the batch result.
+    // iSAM2 settings
     ISAM2Params parameters;
     parameters.relinearizeThreshold = 0.01;
     parameters.relinearizeSkip = 1;
     isam.reset(new ISAM2(parameters));
+
+		// print to confirm reading the YAML file correctly
+		ROS_INFO("cam0/intrinsics exists? %d", nh_ptr->hasParam("cam0/intrinsics")); 
+		ROS_INFO("intrinsics: %d, %d, %d, %d", cam0_intrinsics[0], 
+							cam0_intrinsics[1], cam0_intrinsics[2], cam0_intrinsics[3]);
+    ROS_INFO("Tx: %f", Tx);
+
   }
 
   void callback(const CameraMeasurementConstPtr& camera_msg, const ImuConstPtr& imu_msg) {
   
     vector<FeatureMeasurement> feature_vector = camera_msg->features;  
-   
     ROS_INFO("%lu total features", feature_vector.size());
-		ROS_INFO("Tx: %f", Tx);
 
     for (int i = 0; i < feature_vector.size(); i++) {
 
-			//double d = feature_vector[i].u1 - feature_vector[i].u0; // distance from cam0 to cam1 in pixels
-      //double denom = -d / Tx; 
-			//X = 
+			processFeature(feature_vector[i]);
 
-			Point2 measurement(feature_vector[i].u0, feature_vector[i].v0);
+			if (frame == 0) {
 
-			// graph.emplace_shared<GenericProjectionFactor<Pose3, Point3, Cal3_S2> >(measurement, measurementNoise, Symbol('x', i), Symbol('l', j), K);
-				
+      	// Add a prior on pose x0
+				// 30cm std on x,y,z 0.1 rad on roll,pitch,yaw
+      	noiseModel::Diagonal::shared_ptr poseNoise = noiseModel::Diagonal::Sigmas((Vector(6) << 				Vector3::Constant(0.3),Vector3::Constant(0.1)).finished()); 
+      	graph.emplace_shared<PriorFactor<Pose3> >(Symbol('x', 0), Pose3(), poseNoise);
+
+      	// Should add prior on landmark l0?
+
+			} else {
+
+				// Update iSAM with the new factors
+      	isam->update(graph, initial_estimate);
+
+      	// Each call to iSAM2 update(*) performs one iteration of the iterative nonlinear solver.
+      	// If accuracy is desired at the expense of time, update(*) can be called additional times
+      	// to perform multiple optimizer iterations every step.
+      	isam->update();
+
+				// estimate for current frame
+      	Values current_estimate = isam->calculateEstimate();
+        // Print Results to ROS_INFO
+
+      	// Clear the factor graph and values for the next iteration
+      	graph.resize(0);
+      	initial_estimate.clear();
+			}
+
+			frame++;
 		}
 
-    // Use features u, v image coordinates to estimate feature X, Y, Z locations in camera frame
-    // maybe with openCV, maybe something already in msckf_vio  
-
-    // Create Point3 objects (feature in camera -> feature in world) 
-
-    // Create Pose3 object (estimated camera pose)  
-
-    // Add to Factor Graph
-
-    // Optimize using ISAM
-
-    // Print Results to ROS_INFO
   }
 
-// potential ROS messages
-//ROS_INFO("IMU header: [%s]", imu_msg->header.frame_id);
-//std_msgs::Header header = camera_msg->header;
-//geometry_msgs::Quaternion ori = imu_msg->orientation;
-//ROS_INFO("Camera Coor: [id = %d, u = %f, v = %f]", features[0].id, features[0].u0, features[0].v0);
+	void processFeature(FeatureMeasurement feature) {
+			// identify feature (may appear in previous/future frames)
+			int l = feature.id; 
 
+			double uL = feature.u0;
+			double uR = feature.u1;
+			double v = (feature.v0 + feature.v1) / 2;
+
+			double d = uR - uL;
+			double x = uL;
+			double y = v;
+      double W = -d / Tx;
+
+			// estimated feature location in camera frame
+			double X = (x - cx) / W;
+			double Y = (y - cy) / W;
+			double Z = f / W;
+			
+    	graph.emplace_shared<
+      	  GenericStereoFactor<Pose3, Point3> >(StereoPoint2(uL, uR, v), 
+						noise_model, Symbol('x', frame), Symbol('l', l), K);
+
+			// add initial estimate of landmark if it hasn't appeared yet
+	    if (!initial_estimate.exists(Symbol('l', l))) {
+      		Pose3 camPose = initial_estimate.at<Pose3>(Symbol('x', frame));
+      		Point3 worldPoint = camPose.transform_from(Point3(X, Y, Z)); 
+      		initial_estimate.insert(Symbol('l', l), worldPoint);
+    	}	
+	}
 
 };
 
@@ -172,4 +216,11 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+// potential ROS messages
+//ROS_INFO("IMU header: [%s]", imu_msg->header.frame_id);
+//std_msgs::Header header = camera_msg->header;
+//geometry_msgs::Quaternion ori = imu_msg->orientation;
+//ROS_INFO("Camera Coor: [id = %d, u = %f, v = %f]", features[0].id, features[0].u0, features[0].v0);
+
 
